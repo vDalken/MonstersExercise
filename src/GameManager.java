b73bc567ae461f4d9c5c165bb3d1dd7a8983b287
Fábio <fabio.goncalves@school.mindera.com>
2023-11-22 12:21:14 +0000

    game manager class is now more organized

--- src/GameManager.java
+++ src/GameManager.java
@@ -6,16 +6,36 @@ class GameManager {
     public void startNewGame() {
         System.out.println("\nThe number of monsters you pick has to be the same as your opponent.\n" +
                 "If both of you don't reach a consensus, both will get a default number of 5 monsters each\n");
+        pickNumberOfMonsters(player1, player2);
+        checkIfNumberOfMonstersIsEqual(player1, player2);
+        initializePlayersMonsters(player1, player2);
+        showArrayListOfMonstersOfPlayers(player1, player2);
+        attackingRounds(player1, player2);
+    }
+
+    private void pickNumberOfMonsters(Player player1, Player player2) {
         player1.pickNumberOfMonsters();
         player2.pickNumberOfMonsters();
+    }
+
+    private void checkIfNumberOfMonstersIsEqual(Player player1, Player player2) {
         if (!(player1.getNumberOfMonsters() == player2.getNumberOfMonsters())) {
             player1.setNumberOfMonsters(DEFAULT_NUMBER_OF_MONSTERS);
             player2.setNumberOfMonsters(DEFAULT_NUMBER_OF_MONSTERS);
         }
+    }
+
+    private void initializePlayersMonsters(Player player1, Player player2) {
         player1.initializeMonsters();
         player2.initializeMonsters();
+    }
+
+    private void showArrayListOfMonstersOfPlayers(Player player1, Player player2) {
         player1.showArrayListOfMonsters();
         player2.showArrayListOfMonsters();
+    }
+
+    private void attackingRounds(Player player1, Player player2) {
         do {
             player1.randomizedAttack(player2);
             if (player2.getMonsters().isEmpty()) {
@@ -32,5 +52,4 @@ class GameManager {
             System.out.println("\nPlayer 2 won");
         }
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main{
     public static void main(String[] args){
         GameManager game = new GameManager();
         game.startNewGame();
-
     }
 }
